0024cf0bbe6f91fef51ffbaa63245d578d33b86e
landstring <landstring@gmail.com>
2023-07-28 23:18:11 +0400

    Initial commit

---
+++ src/main/java/com/example/cfnote/CfNoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.cfnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cfnote/CfNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cfnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
